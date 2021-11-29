398567f4f5f4fe8667c28457a08bf28ad1c70d65
Nicholas DuChane <nduchane@ivytech.edu>
2021-11-29 06:03:16 -0500

    set up initial file structure

---
+++ app.js
@@ -0,0 +1,27 @@
+ // set up node module requirements
+const express = require('express');
+
+// express app (sets "app" to be used with express)
+const app = express(); 
+
+ //register view engine
+ app.set('view engine', 'ejs');
+
+ // listen for requests
+ app.listen(3000);
+
+ // function to search for "index" as an ejs type
+ app.get('/',(req,res) => {
+    res.render('index');
+ });
+
+ // route any other sites here
+
+
+ /* keep this file on bottom
+ if site cannot find any of the above 
+ then default to this 404 page.
+ Also sets the error status to 404*/
+ app.use((req,res) => {
+    res.status(404).render('404');
+  });
